986e60362e3c6f1ff219825c3bb5f283e94d904a
katanaevav <katanaev@gmail.com>
2020-03-15 22:41:10 +0400

    Добавлен модуль выноса окна на передний план при клике

---
+++ js/clickWindow.js
@@ -0,0 +1,15 @@
+'use strict';
+
+var windowContainer = document.querySelector('.windows');
+
+var onWindowClick = function (evt) {
+  var windowsList = windowContainer.querySelectorAll('.window');
+  windowsList.forEach(function (item, index) {
+    item.style.zIndex = 0;
+    if (item.contains(evt.target)) {
+      item.style.zIndex = 1;
+    }
+  });
+}
+
+windowContainer.addEventListener('mousedown', onWindowClick);
