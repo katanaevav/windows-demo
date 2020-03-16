73fcd3170d66ac196c6c7b071e25189db6cf1226
katanaevav <katanaev@gmail.com>
2020-03-16 17:04:37 +0400

    Добавлено изменение размеров окон

--- js/setupWindow.js
+++ js/setupWindow.js
@@ -1,5 +1,6 @@
 'use strict';
-
+var MIN_WINDOW_HEIGHT = 260;
+var MIN_WINDOW_WIDTH = 360;
 var windowNumber = 0;
 
 var onMoveWindow = function (evt, wnd, draggedItem) {
@@ -26,8 +27,16 @@ var onMoveWindow = function (evt, wnd, draggedItem) {
         y: moveEvt.clientY
       };
 
-      wnd.style.top = (wnd.offsetTop - shift.y) + 'px';
-      wnd.style.left = (wnd.offsetLeft - shift.x) + 'px';
+      var top = (wnd.offsetTop - shift.y);
+      var left = (wnd.offsetLeft - shift.x);
+
+      top = (top < 0) ? 0 : top;
+      left = (left < 0) ? 0 : left;
+      top = (top > wnd.offsetParent.offsetHeight - wnd.offsetHeight) ? wnd.offsetParent.offsetHeight - wnd.offsetHeight : top;
+      left = (left > wnd.offsetParent.offsetWidth - wnd.offsetWidth) ? wnd.offsetParent.offsetWidth - wnd.offsetWidth : left;
+
+      wnd.style.top = top + 'px';
+      wnd.style.left = left + 'px';
     };
 
     var onMouseUp = function (upEvt) {
@@ -47,7 +56,61 @@ var onMoveWindow = function (evt, wnd, draggedItem) {
     
     document.addEventListener('mousemove', onMouseMove);
     document.addEventListener('mouseup', onMouseUp);
+};
+
+var onResizeWindow = function (evt, wnd, resizeItem) {
+  evt.preventDefault();
+
+  var startCoords = {
+    x: evt.clientX,
+    y: evt.clientY
+  };
+
+  var dragged = false;
+
+  var onMouseMove = function (moveEvt) {
+    moveEvt.preventDefault();
+    dragged = true;
+
+    var shift = {
+      x: startCoords.x - moveEvt.clientX,
+      y: startCoords.y - moveEvt.clientY
+    };
+
+    startCoords = {
+      x: moveEvt.clientX,
+      y: moveEvt.clientY
+    };
+
+    var height = (wnd.offsetHeight  - shift.y);
+    var width = (wnd.offsetWidth - shift.x);
+
+    height = (height < MIN_WINDOW_HEIGHT) ? MIN_WINDOW_HEIGHT : height;
+    width = (width < MIN_WINDOW_WIDTH) ? MIN_WINDOW_WIDTH : width;
+    height = (height > wnd.offsetParent.offsetHeight - wnd.offsetTop) ? wnd.offsetParent.offsetHeight - wnd.offsetTop : height;
+    width = (width > wnd.offsetParent.offsetWidth - wnd.offsetLeft) ? wnd.offsetParent.offsetWidth - wnd.offsetLeft : width;
+
+    wnd.style.height = height + 'px';
+    wnd.style.width = width + 'px';
+  };
+
+  var onMouseUp = function (upEvt) {
+    upEvt.preventDefault();
+    document.removeEventListener('mousemove', onMouseMove);
+    document.removeEventListener('mouseup', onMouseUp);
+
+    if (dragged) {
+      var onClickPreventDefault = function (clickEvt) {
+        clickEvt.preventDefault();
+        resizeItem.removeEventListener('click', onClickPreventDefault);
+      };
+
+      resizeItem.addEventListener('click', onClickPreventDefault);
+    }
+  };
   
+  document.addEventListener('mousemove', onMouseMove);
+  document.addEventListener('mouseup', onMouseUp);
 };
 
 var setWindowClosable = function (wnd) {
@@ -66,6 +129,12 @@ var setWindowMovable = function (wnd, draggedItem) {
   });
 };
 
+var setWindowResizeable = function (wnd, resizeItem) {
+  resizeItem.addEventListener('mousedown', function (evt) {
+    onResizeWindow(evt, wnd, resizeItem);
+  });
+}
+
 var newWindowButton = document.querySelector('.manage-windows__new-window');
 
 var onCreaneNewWindow = function () {
@@ -83,6 +152,7 @@ var onCreaneNewWindow = function () {
   windowsContainer.appendChild(fragment);
 
   setWindowMovable(newWindow, newWindow.querySelector('.window-header'));
+  setWindowResizeable(newWindow, newWindow.querySelector('.window-status__resize-element'))
   setWindowClosable(newWindow);
 };
 
